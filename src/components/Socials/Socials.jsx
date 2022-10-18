e4dbfa8e44edcd79c0b5d95cab93a41aa5730354
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-18 16:28:00 +0300

    u

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,10 +1,38 @@
-import css from './Socials.module.css'
-import svg from '../../images/symbol-defs.svg'
-export const Socials=()=>{
-    return(
-        <div className={css.socials}>
-            <a href="" type='tel'></a>
-            +380 93 720 80 10 
-        </div>
-    )
-}
+import css from './Socials.module.css';
+import svg from '../../images/symbol-defs.svg';
+export const Socials = () => {
+  return (
+    <div className={css.socials}>
+      
+      <ul className={css.list}>
+        <li className={css.item}>
+          {' '}
+          <a className={css.link} href="https://www.instagram.com/whataivan/">
+            <svg className={css.icon}>
+              <use href={`${svg}#icon-instagram`} />
+            </svg>
+          </a>
+        </li>
+        <li className={css.item}>
+          {' '}
+          <a className={css.link} href="https://www.instagram.com/whataivan/">
+            <svg className={css.icon}>
+              <use href={`${svg}#icon-telegram`} />
+            </svg>
+          </a>
+        </li>
+        <li className={css.item}>
+          {' '}
+          <a className={css.link} href="https://www.instagram.com/whataivan/">
+            <svg className={css.icon}>
+              <use href={`${svg}#icon-github`} />
+            </svg>
+          </a>
+        </li>
+      </ul>
+      <a className={css.tel} href="+380937208010" type="tel">
+        +380 93 720 80 10
+      </a>
+    </div>
+  );
+};
