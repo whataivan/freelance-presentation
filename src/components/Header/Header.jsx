48d2b334db9f43b7fad3e0647d5aeb6ea2c228b3
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-21 04:47:13 +0300

    u

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import css from './Header.module.css';
 
 export const Header = ({yOffset}) => {
   return (
-    <div style={ yOffset>800?{ backdropFilter: "blur(5px)",  }:{background: '#3f3c3c', } } className={css.header}>
+    <div style={ yOffset>800?{ backdropFilter: "blur(5px)",  }:{backdropFilter: "blur(5px)", } } className={css.header}>
       <a href="./" className={css.logo}>
       {/* <svg  className={css.icon}>
               <use href={`${svg}#icon-head`} />
