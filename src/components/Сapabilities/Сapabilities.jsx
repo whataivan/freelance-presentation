f34153cfd41d44ab6e539403ade652d2f6ae6200
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-22 02:22:36 +0300

    u

--- src/components/Сapabilities/Сapabilities.jsx
+++ src/components/Сapabilities/Сapabilities.jsx
@@ -1,23 +1,31 @@
 import css from './Сapabilities.module.css';
 import svg from '../../images/symbol-defs.svg';
-
-
+import { useState } from 'react';
 
 export const Сapabilities = () => {
+  const [leftActive, setLeftActive] = useState(false);
+  const [rightActive, setRightActive] = useState(false);
   return (
     <div className={css.capabilities}>
       <div className={css.leftSide}>
-        <h3 className={css.title}>
-        <svg  className={css.icon}>
-              <use href={`${svg}#icon-check`} />
-            </svg>
-            Что я могу</h3>
+        <h3 onMouseEnter={()=>setRightActive(true)} onMouseLeave={()=>setRightActive(false)} className={css.title}>
+          <svg className={css.icon}>
+            <use href={`${svg}#icon-check`} />
+          </svg>
+          Что я могу
+        </h3>
       </div>
       <div className={css.rightSide}>
-      <h3 className={css.titleSecond}>
-      <svg  className={css.iconSecond}>
-              <use href={`${svg}#icon-cross`} />
-            </svg>Чего я не могу</h3>
+        {rightActive ? (
+          <p className={css.rightSideText}>wdqSDSdsfsdfszdfs sSSWFVD dfdddd </p>
+        ) : (
+          <h3 className={css.titleSecond}>
+            <svg className={css.iconSecond}>
+              <use href={`${svg}#icon-cross`}/>
+            </svg>
+            Чего я не могу
+          </h3>
+        )}
       </div>
     </div>
   );
