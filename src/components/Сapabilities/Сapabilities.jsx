dc4e6e0ba26c4c878f5b7995c5208901151f05a8
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-10-22 15:36:30 +0300

    u

--- src/components/Сapabilities/Сapabilities.jsx
+++ src/components/Сapabilities/Сapabilities.jsx
@@ -1,13 +1,14 @@
 import css from './Сapabilities.module.css';
 import svg from '../../images/symbol-defs.svg';
 import { useState } from 'react';
-
+// import th from "../../images/light.png"
 export const Сapabilities = () => {
 //   const [leftActive, setLeftActive] = useState(false);
   const [rightActive, setRightActive] = useState(false);
   return (
     <div className={css.capabilities}>
       <div className={css.leftSide}>
+        {/* <img src={th} alt="" /> */}
         <h3 onMouseEnter={()=>setRightActive(true)} onMouseLeave={()=>setRightActive(false)} className={css.title}>
           <svg className={css.icon}>
             <use href={`${svg}#icon-check`} />
