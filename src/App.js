38ba8dbb242b18038c92cdebbee1e82173adb3b8
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2024-02-08 21:46:47 +0200

    fdgdf

--- src/App.js
+++ src/App.js
@@ -6,23 +6,49 @@ import { Socials } from 'components/Socials/Socials';
 import { Hero } from 'components/Hero/Hero';
 import {Сapabilities} from './components/Сapabilities/Сapabilities'
 // import { Par } from 'components/Par/Par';
+
 import { useEffect, useState } from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { Portfolio } from 'components/Portfolio/Portfolio';
 import { UpBtn } from 'components/UpBtn/UpBtn';
+import mys
 
 //-----------------------------------------------------------------------------------
 export function App() {
   const [yOffset, SetYOffset] = useState(0)
-
+  const [reviews, SetReviews] = useState(0)
   const onScroll = (e) => {
     SetYOffset(e.currentTarget.pageYOffset)
   }
 
   useEffect(() => {
     window.addEventListener('scroll', onScroll)
+    dbChange(false)
     return () => (window.removeEventListener('scroll', onScroll))
   }, [yOffset]);
+
+  const sql = require('mysql')
+
+  const connection = sql.createConnection({
+      host: "ga477435.mysql.tools",
+      user: "ga477435_46",
+      database: "ga477435_46",
+      password: "a*85F)iuU7"
+  
+  })
+  async function dbChange(set=false, keys, values,  ) {
+     
+      const query = set?(`INSERT INTO revs (${keys.join(',')}) VALUES (${values.map(el=>"'"+el+"'").join(',')});`):('SELECT * FROM revs')
+     
+      connection.query(query, (err, res, fields)=>{
+          if (err) {
+              console.log(err);
+          }else{
+              console.log(res);
+              SetReviews(res)
+          }
+      })
+  }
 
   return (
     <ParallaxProvider>
